b3d60e2e008fd61d8c04f03170fe9b238146bfc4
Vinaykumar-shettar <68851137+Vinaykumar-shettar@users.noreply.github.com>
2020-08-03 16:58:21 +0530

    Create vehicletelematics.h

---
+++ vehicletelematics.h
@@ -0,0 +1,19 @@
+typede enum{
+MOTOR_TEMP,
+BATTREY_PC,
+BATTERY_TEMP
+}vehicle_param;
+
+typedef struct{
+int vehicle_id;
+vehicle_param param_id;
+float param_val;
+}vehicle_param;
+
+tpedef struct
+{
+int vehicle_id;
+float motor_temp;
+float battery_pc;
+float battery_temp;
+}vehicle_telematics;
