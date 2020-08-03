449b19be6a4a7e9ebf4e8eb1b893670db7e3b129
Vinaykumar-shettar <68851137+Vinaykumar-shettar@users.noreply.github.com>
2020-08-03 18:05:55 +0530

    Update vehicletelematics.h

--- vehicletelematics.h
+++ vehicletelematics.h
@@ -5,9 +5,9 @@ BATTERY_TEMP
 }vehicle_param;
 
 typedef struct{
-int vehicle_id;
-vehicle_param param_id;
-float param_val;
+int           vehicle_id;
+vehicle_param id;
+float         param_val;
 }vehicle_info;
 
 tpedef struct
@@ -17,3 +17,5 @@ float motor_temp;
 float battery_pc;
 float battery_temp;
 }vehicle_telematics;
+
+bool update_vehicle_info(vehicle_telematics vehicleinfo);
